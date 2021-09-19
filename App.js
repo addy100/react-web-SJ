95ad3ce2bd170b6b52967600f4c413e39d4e1c8d
Vansh Jain <77625835+vanshjain0205@users.noreply.github.com>
2021-09-19 21:29:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,146 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div id="main-container" class="main-container nav-effect-1">
+     <div class="main clearfix">
+     <div class="page-container">
+     <section id="book_list">
+
+      
+
+<div class="grid-shuffle">
+  <ul id="grid" class="row">
+
+    <li class="book-item small-12 medium-6 columns" data-groups='["classic","Sem-2","Sem-4","Sem-5","Sem-6","Sem-7","Sem-8"]' data-date-created='1937' data-title='Of Mice and Men' data-color='#fcc278'>
+      <div class="bk-img">
+        <div class="bk-wrapper">
+          <div class="bk-book bk-bookdefault">
+            <div class="bk-front">
+              <div class="bk-cover" style={{backgroundColor:'cyan'}}></div>
+            </div>
+            <div class="bk-back"></div>
+            <div class="bk-left"></div>
+          </div>
+        </div>
+      </div>
+      <div class="item-details">
+        <h3 class="book-item_title">Electrical Engineering</h3>
+        <a href="resources.html" class="button ">Details</a>
+      </div>
+      <div class="overlay-details">
+                <a href="#" class="close-overlay-btn">Close</a>
+                
+                <div class="overlay-desc activated">
+                  <h2 class="overlay_title">Choose Your Semester</h2>
+                  <p class="author">by L. Frank Baum</p>
+                  <p class="published">1900</p>
+                  <p class="synopsis">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam luctus convallis sem. Nunc sed tristique augue. Aenean at nulla vel lacus volutpat bibendum vitae ut nibh. Aliquam eu metus et purus rutrum malesuada. Aenean in auctor mauris,
+                    non vulputate libero. Nullam auctor, purus ut cursus convallis, lectus tellus dignissim lectus, id tempor ipsum leo ut nulla. Vestibulum vitae elit erat.</p>
+                  <a href="#" class="button preview">Preview</a>
+                </div>
+                
+              </div>
+    </li>
+
+    <li class="book-item small-12 medium-6 columns" data-groups='["classic","Sem-2","Sem-3","Sem-4","Sem-5","Sem-6","Sem-7","Sem-8"]' data-date-created='1951' data-title='The Catcher in the Rye' data-color='#009c8b'>
+      <div class="bk-img">
+        <div class="bk-wrapper">
+          <div class="bk-book bk-bookdefault">
+            <div class="bk-front">
+              <div class="bk-cover" style={{backgroundColor:'orange'}}></div>
+            </div>
+            <div class="bk-back"></div>
+            <div class="bk-left"></div>
+          </div>
+        </div>
+      </div>
+      <div class="item-details">
+        <h3 class="book-item_title">COE</h3>
+        <a href="#" class="button ">Details</a>
+      </div>
+    </li>
+
+    <li class="book-item small-12 medium-6 columns" data-groups='["classic","Sem-2","Sem-3","Sem-4","Sem-5"]' data-date-created='1949' data-title='1984' data-color='#9c9688'>
+      <div class="bk-img">
+        <div class="bk-wrapper">
+          <div class="bk-book bk-bookdefault">
+            <div class="bk-front">
+              <div class="bk-cover" style={{backgroundColor:'pink'}}></div>
+            </div>
+            <div class="bk-back"></div>
+            <div class="bk-left"></div>
+          </div>
+        </div>
+      </div>
+      <div class="item-details">
+        <h3 class="book-item_title">CSAI</h3>
+        <a href="#" class="button ">Details</a>
+      </div>
+      </li>
+
+    <li class="book-item small-12 medium-6 columns" data-groups='["classic","Sem-2","Sem-3"]' data-date-created='1954' data-title='Lord of the Flies' data-color='#db2e0f'>
+      <div class="bk-img">
+        <div class="bk-wrapper">
+          <div class="bk-book bk-bookdefault">
+            <div class="bk-front">
+              <div class="bk-cover" style={{backgroundColor:'red'}}></div>
+            </div>
+            <div class="bk-back"></div>
+            <div class="bk-left"></div>
+          </div>
+        </div>
+      </div>
+      <div class="item-details">
+        <h3 class="book-item_title">CSDS</h3>
+        <a href="#" class="button ">Details</a>
+      </div>
+    </li>
+
+    <li class="book-item small-12 medium-6 columns" data-groups='["classic","Sem-2","Sem-3","Sem-4","Sem-5","Sem-6","Sem-7","Sem-8"]' data-date-created='1925' data-title='The Great Gatsby' data-color='#e3b005'>
+      <div class="bk-img">
+        <div class="bk-wrapper">
+          <div class="bk-book bk-bookdefault">
+            <div class="bk-front">
+              <div class="bk-cover" style={{backgroundColor:'yellow'}}></div>
+            </div>
+            <div class="bk-back"></div>
+            <div class="bk-left"></div>
+          </div>
+        </div>
+      </div>
+      <div class="item-details">
+        <h3 class="book-item_title">Electrical</h3>
+        <a href="#" class="button ">Details</a>
+      </div>
+    </li>
+    <li class="book-item small-12 medium-6 columns" data-groups='["classic","Sem-2","Sem-3","Sem-4","Sem-5","Sem-6","Sem-7","Sem-8"]' data-date-created='1851' data-title='Moby Dick' data-color='#a9afad'>
+      <div class="bk-img">
+        <div class="bk-wrapper">
+          <div class="bk-book bk-bookdefault">
+            <div class="bk-front">
+              <div class="bk-cover" style={{backgroundColor:'purple'}}></div>
+            </div>
+            <div class="bk-back"></div>
+            <div class="bk-left"></div>
+          </div>
+        </div>
+      </div>
+      <div class="item-details">
+        <h3 class="book-item_title">ECE</h3>
+        <a href="#" class="button ">Details</a>
+      </div>
+    </li>
+    </ul>
+     </div>
+     </section>
+     </div>
+     </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
